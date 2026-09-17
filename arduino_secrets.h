ddf7de5ab0ff5d6b2dc0fad92dd98848ffa3eb26
Richard <richvern@orange.fr>
2026-09-17 17:26:14 +0200

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,12 @@
+#define SECRET_SSID  "nom_du_reseau_1"
+#define SECRET_PASS  "mot_de_passe_1"
+
+#define SECRET_SSID2 "nom_du_reseau_2"
+#define SECRET_PASS2 "mot_de_passe_2"
+
+// Optionnel : décommentez pour un 3e réseau connu
+// #define SECRET_SSID3 "nom_du_reseau_3"
+// #define SECRET_PASS3 "mot_de_passe_3"
+
+// Fortement recommandé : mot de passe pour sécuriser les mises à jour OTA
+#define SECRET_OTA_PASSWORD "votre_mot_de_passe_ota"
